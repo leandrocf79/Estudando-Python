474b31073da3cce9527b259655043bba2d511315
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-04-12 09:39:02 -0300

    Add files via upload

---
+++ Repeticao com substituicao.py
@@ -0,0 +1,14 @@
+numero = -1
+while numero <= 8:
+    if numero == 3:
+        numero += 1
+        print("Tres")
+        continue
+    elif numero == 5:
+        numero += 1
+        print("Cinco")
+        continue
+
+    print(numero)
+    numero += 1
+print("fim")
