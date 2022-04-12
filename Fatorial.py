15b06cfc063d635ce3f69e55bebba7179efb99cb
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-04-12 11:52:51 -0300

    Update Fatorial.py

--- Fatorial.py
+++ Fatorial.py
@@ -1,8 +1,20 @@
-num=eval(input("Digite um número fatorial: "))
+import time
+Num= eval(input("Digite um número fatorial: "))
+while (Num<=0 or Num>10):                          # limitar exageros dos usuários
+    Num = eval(input("Digite um número fatorial: "))
 
-fatorial=1
-voltas=1
-while voltas <= num:
-    fatorial*=voltas
-    print(fatorial)
-    voltas+=1
+Fatorial = 1
+Voltas = 1
+while Voltas <= Num:
+    Fatorial *= Voltas
+    #print(Fatorial) # imprime todos os resultados na tela
+    #time.sleep(0.5) # apenas para teste de aprendizagem do programador
+    Voltas += 1
+
+    if Voltas == Num:  # vai imprimir na tela apenas o resultado final
+        print(Fatorial)
+
+while True:
+    Saida = str(input('\nPressione "ENTER" para sair '))
+    if not Saida:
+        exit()
