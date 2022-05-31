6136bccfa46a3566cc6eb3db206362c1504f0a54
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-05-31 17:33:34 -0300

    Add files via upload

---
+++ 1- Pilha - Conversor_Decimal_Binario.py
@@ -0,0 +1,28 @@
+class Pilha():
+    def __init__(self):
+        self.data=[]
+
+    def push(self, x):    #insere elementos
+        self.data.append(x)
+
+    def pop(self):      # remove um elemento da pilha
+        if len(self.data) >0:
+            return self.data.pop(-1) # O -1 REMOVE o último elemento da lista
+
+    def top(self):          # vai verificar se tem pelo menos um elemento inserido
+        if len(self.data) > 0:
+            return self.data[-1] # O -1 está consultando o último elemento da lista
+
+    def empty(self):  # Verifica se a pilha está vazia
+        return not len(self.data) >0
+
+p=Pilha()
+num= int(input("Digite um número decimal para converter para binário: "))
+while num > 0:
+    resto=num %2
+    num=num//2
+    p.push(resto)
+
+# desemplilhar
+while not p.empty():
+    print(p.pop())
