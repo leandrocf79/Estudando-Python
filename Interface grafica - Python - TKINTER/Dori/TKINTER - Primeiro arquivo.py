f29f2c0c062f86dc28be9cdf1cfe56ec7ab61bca
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-06-20 17:23:54 -0300

    Add files via upload

---
+++ Interface grafica - Python - TKINTER/Dori/TKINTER - Primeiro arquivo.py
@@ -0,0 +1,17 @@
+from tkinter import Tk, Label, PhotoImage, TOP, BOTTOM
+#from tkinter import *
+
+root = Tk ()
+
+photo = PhotoImage(file = 'Dori.gif').subsample(2)  #subsample: a cada 3px vai pegar 1px da
+                                                   # imagem origial. Serve para diminir o tamanho
+hello = Label( master = root, image=photo, width=500, height=370)
+imagem =Label (master = root, image = photo)
+imagem.pack(side=TOP) # local onde deve ser inserida a imagem
+
+texto = Label(master=root, font= ("Cooper Black", 18), text = "Olá pessoal!")
+# hello = Label( master = root, text= "Olá pessoal!")
+# hello.pack()
+texto.pack(side = BOTTOM)
+
+root.mainloop()
